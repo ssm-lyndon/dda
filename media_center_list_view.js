d49ed88212790a6a9593092e6ab7c46189464508
ssm-lyndon <lyndon@speedshiftmedia.com>
2017-03-29 11:59:27 -0700

    Expanded functionality
    Added functionality to convert just about any "Edit" button found throughout DDA to open itself in a new tab. Won't work on the Advertisers list view, however.

--- media_center_list_view.js
+++ media_center_list_view.js
@@ -1,51 +1,51 @@
-const scriptVersion = '2.0';
-let updateMessage = ''
+/* If Advertisers page */
+if (window.location.pathname === '/advertisers.html') {
+  alert("Sorry, ButtonMe won't work on the Advertisers page.");
+}
+else {
 
-updateMessage += 'Media Center List View Helper upgraded to version 2.0\n\n';
-updateMessage += 'Changes:\n';
-updateMessage += '===========\n';
-updateMessage += '+ Removed Tear Sheets button (which has been defunct for awhile)\n';
-updateMessage += '+ Added button to go directly to the Conversions step\n';
-updateMessage += '+ Added auto-updates functionality';
+  /* If Media Center */
+  if (window.location.pathname === '/products/dynamicwebads/mediacenter/home.html') {
 
-/* check script version; alert the user if there have been updates since they last used this */
-$.getScript('https://cdnjs.cloudflare.com/ajax/libs/js-cookie/2.1.3/js.cookie.min.js')
-  .done(function(script, textStatus) {
-    if (Cookies.get('media_center_list_view_scriptVersion') !== scriptVersion) {
-      console.log(updateMessage);
+    function newButton(e, t) {
+      let n = document.createElement('a');
+      n.innerHTML = e;
+      n.href = t;
+      n.target = '_blank';
+      n.style.background = '#fff';
+      n.style.border = '1px solid #f0f';
+      n.style.font = 'bold 10px Lucida Grande';
+      n.style.margin = '2px';
+      n.style.padding = '2px';
+      return n;
+    };
 
-      Cookies.set('media_center_list_view_scriptVersion', scriptVersion, 365);
+    let buttons = document.getElementsByClassName('step_save_small');
+    for (let i = 0; i < buttons.length; i++) {
+      let button = buttons[i],
+        parent = button.parentNode;
+      if (button.getAttribute('value') == 'Edit') {
+        let link = buttons[i].getAttribute('onclick');
+        link = link.replace(/document.location.href='/gi, '');
+        link = link.replace(/';/gi, '');
+        parent.innerHTML = '';
+        parent.appendChild(new newButton('Edit', link.replace(/Action=details/gi, 'Action=details')));
+        parent.appendChild(new newButton('Appearance', link.replace(/Action=details/gi, 'Action=appearance')));
+        parent.appendChild(new newButton('Copy', link.replace(/Action=details/gi, 'Action=copyprefs')));
+        parent.appendChild(new newButton('Data', link.replace(/Action=details/gi, 'Action=dataprefs')));
+        parent.appendChild(new newButton('Conversions', link.replace(/Action=details/gi, 'Action=conversions')));
+        parent.appendChild(new newButton('Preview', link.replace(/Action=details/gi, 'Action=preview')));
+      };
     }
-  })
-;
+  }
 
-function newButton(e, t) {
-  let n = document.createElement('a');
-  n.innerHTML = e;
-  n.href = t;
-  n.target = '_blank';
-  n.style.background = '#fff';
-  n.style.border = '1px solid #777';
-  n.style.font = 'bold 10px Lucida Grande';
-  n.style.margin = '2px';
-  n.style.padding = '2px';
-  return n;
-};
-
-let buttons = document.getElementsByClassName('step_save_small');
-for (let i = 0; i < buttons.length; i++) {
-  let button = buttons[i],
-    parent = button.parentNode;
-  if (button.getAttribute('value') == 'Edit') {
-    let link = buttons[i].getAttribute('onclick');
-    link = link.replace(/document.location.href='/gi, '');
-    link = link.replace(/';/gi, '');
-    parent.innerHTML = '';
-    parent.appendChild(new newButton('Edit', link.replace(/Action=details/gi, 'Action=details')));
-    parent.appendChild(new newButton('Appearance', link.replace(/Action=details/gi, 'Action=appearance')));
-    parent.appendChild(new newButton('Copy', link.replace(/Action=details/gi, 'Action=copyprefs')));
-    parent.appendChild(new newButton('Data', link.replace(/Action=details/gi, 'Action=dataprefs')));
-    parent.appendChild(new newButton('Conversions', link.replace(/Action=details/gi, 'Action=conversions')));
-    parent.appendChild(new newButton('Preview', link.replace(/Action=details/gi, 'Action=preview')));
-  };
+  /* all other pages */
+  else {
+    $('.step_save_small[value="Edit"]').each(function(key, node) {
+      var action = $(node).attr('onclick');
+      action = action.replace("document.location.href='", '');
+      action = action.replace("';", '');
+      $(node).parent().html('<a href="'+action+'" target="_blank" style="background-color:#fff;border:1px solid #f0f;font-size:10px;font-weight:bold;padding:2px;4px">Edit</a>');
+    });
+  }
 }
